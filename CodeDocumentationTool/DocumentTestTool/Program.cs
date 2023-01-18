00e77e0be7f57c708368ae2dc1dce915e76c5760
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2023-01-18 01:48:46 +0100

    Update Program.cs

--- CodeDocumentationTool/DocumentTestTool/Program.cs
+++ CodeDocumentationTool/DocumentTestTool/Program.cs
@@ -1,40 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Reflection;
-using System.Text;
+using CodeDocumentationTool;
 
-namespace CodeDocumentationTool
+namespace DocumentTestTool
 {
-    public class ShowCodeDocuments
+    public class Program
     {
-        public void GetDocs()
+
+        static void Main(string[] args)
         {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            foreach (var assembly in assemblies)
-            {
-                {
-                    var types = assembly.GetTypes();
-                    foreach (var type in types)
-                    {
-                        var members = type.GetMembers();
-                        foreach (var member in members)
-                        {
-                            var attributes = member.GetCustomAttributes(typeof(DocumentAttribute), false);
-                            if (attributes.Length > 0)
-
-                            {
-
-                                Console.WriteLine("Type: {0}", member.MemberType);
-                                Console.WriteLine("Name: {0}", member.Name);
-                                Console.WriteLine($"Description: {((DocumentAttribute)attributes[0]).Description}");
-                                Console.WriteLine();
-                            }
-                        }
-                    }
-                }
-            }
+            ShowCodeDocuments doc = new ShowCodeDocuments();
+                doc.GetDocs();
+          
+        
         }
     }
 }
