d6c71533539d8094712f6afda7c87457e7712ef3
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2023-01-18 13:34:02 +0100

    Update DocumentAttribute.cs

--- CodeDocumentationTool/CodeDocumentationTool/DocumentAttribute.cs
+++ CodeDocumentationTool/CodeDocumentationTool/DocumentAttribute.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Collections.Generic;
 using System.Text;
@@ -34,106 +34,33 @@ namespace CodeDocumentationTool
         {
             [Document(Description = "This assigns value to the Name field",
             Input = "This is a field which is the Name of female software developers and the data type is  string")]
-            FemaleDevelopers(string name)
+             public FemaleDevelopers(string name)
             {
                 Name = name;
             }
             [Document(Description = "This is a property, it sets and gets  the names of the software developers")] public string Name { get; set; }
             [Document(Description = "This is also a property, it sets and gets  their departments")] public string Department { get; set; }
-            [Document(Description = "This describes what happens in each department",Input ="It takes a string department",Output="It returns their department")]
-            public string SampleCode(string department,string name)
+            [Document(Description = "This describes what happens in each department", Input = "It takes a string department", Output = "It returns their department")]
+            public string SampleCode(string department, string name)
             {
                 return name + " is a member of " + Departments.OS;
 
             }
 
-            [Document(Description ="This contains enumerable for the different departments")]
-        public enum Departments
-        {
-            OS,
-            Networking,
-            Finance
-        }
-
-    }
-
-    internal class Program
-    {
-
-        public static void GetMethods(Type type)
-        {
-
-
-            MethodInfo[] getmeth = type.GetMethods();
-            for (int i = 0; i < getmeth.GetLength(0); i++)
+            [Document(Description = "This contains enumerable for the different departments")]
+            public enum Departments
             {
-                object[] methinfo = getmeth[i].GetCustomAttributes(true);
-
-                foreach (Attribute doc in methinfo.Cast<Attribute>())
-                {
-                    if (doc is DocumentAttribute)
-                    {
-                        DocumentAttribute attribute = (DocumentAttribute)doc;
-
-                        Console.WriteLine(attribute.Description);
-                    }
-                }
-            }
-        }
-        public static void GetClass(Type type)
-        {
-            Console.WriteLine("Assembly: {0}", Assembly.GetExecutingAssembly());
-            object[] classinfo = type.GetCustomAttributes(true);
-            foreach (object attr in classinfo)
-            {
-                if (attr is DocumentAttribute)
-                {
-                    DocumentAttribute attribute = (DocumentAttribute)attr;
-
-                    Console.WriteLine($"{attribute.Description} {attribute.Input}");
-                }
+                OS,
+                Networking,
+                Finance
             }
 
-        }
-        public static void GetProps(Type type)
-        {
-            PropertyInfo[] propinfo = type.GetProperties();
-            for (int i = 0; i < propinfo.Length; i++)
-            {
-                object[] prop = propinfo[i].GetCustomAttributes(true);
-
-                foreach (Attribute myprop in prop.Cast<Attribute>())
-                {
-                    if (myprop is DocumentAttribute)
-                    {
-                        DocumentAttribute attribute = (DocumentAttribute)myprop;
-
-                        Console.WriteLine(attribute.Description);
-                    }
-                }
-
-            }
-
-        }
-        public static void GetDocs(Type type)
-        {
-            GetClass(type);
-            GetMethods(type);
-            GetProps(type);
-        }
-
-
-
-        static void Main(string[] args)
-        {
-            Program.GetDocs(typeof(SchoolEnum));
-
-            SampleClass sam = new SampleClass();
-
-            Console.WriteLine(sam.SampleMethod("stanley"));
         }
     }
 }
+
+    
+
 
 
 
